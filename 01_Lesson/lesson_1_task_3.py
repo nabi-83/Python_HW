9e93b2d058b9c622968504f29798b651564474b7
nabi-83 <nabi-83@mil.ru>
2025-10-03 13:23:13 +0500

    01_Lesson

---
+++ 01_Lesson/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = "Ирина Зуфаровна"
+last_name = "Бухарина "
+input("Вас зовут: " + last_name + first_name)
---
+++ 01_Lesson/lesson_1_task_4.py
@@ -0,0 +1,4 @@
+def print_greeting ():
+    print ("Приет, Мир!")
+
+print_greeting ()
