92513eae70815c84e4ff284a6ae99110f99d356b
nabi-83 <nabi-83@mil.ru>
2025-10-05 19:56:53 +0500

    02_Lesson

--- Lesson2/02_lesson/lesson_2_task_2.py
+++ Lesson2/02_lesson/lesson_2_task_2.py
@@ -1,7 +1,7 @@
-def is_year_leap (год):
-   if год % 4 == 0:
+def is_year_leap (year):
+   if year % 4 == 0:
         return(True)
    else:
         return(False)
-год = int(input("Введите номер года: "))
-print(f"год {год}: {is_year_leap (год)}")
+year = int(input("Введите номер года: "))
+print(f"год {year}: {is_year_leap (year)}")
