5c1efc6978ceffb95b963c8f5c5622b2638c3864
nabi-83 <nabi-83@mil.ru>
2025-10-06 16:24:33 +0500

    02_Lesson

---
+++ Lesson2/02_lesson1/lesson_2_task_3.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+
+def square(x):
+    s = x**2
+    return s
+
+
+x = float(input("Длина стороны квадрата: "))
+result = square(x)
+rounded = ceil(result)
+print(f"Площадь квадрата: {rounded}")
